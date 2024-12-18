dc721bb5dc07926c0f42c15ac5cb8ca4747ff39e
alexwindelberg <alexwindelberg@gmail.com>
2024-12-18 12:06:12 -0500

    translations added to the project

---
+++ src/i18n/translations/language.ts
@@ -0,0 +1,12 @@
+export const languageTranslations = {
+  en: {
+    "language": "Language",
+    "language.en": "English",
+    "language.es": "Spanish"
+  },
+  es: {
+    "language": "Idioma",
+    "language.en": "Inglés",
+    "language.es": "Español"
+  }
+};
---
+++ src/i18n/translations/navigation.ts
@@ -0,0 +1,14 @@
+export const navigationTranslations = {
+  en: {
+    "nav.home": "Home",
+    "nav.properties": "Properties",
+    "nav.about": "About",
+    "nav.contact": "Contact",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.properties": "Propiedades",
+    "nav.about": "Sobre Mí",
+    "nav.contact": "Contacto",
+  }
+};
